bed48fc173f237c0bd619e8e23ab3c0f011bb15d
RuaaBaniOdh <152775685+ruaamuhmaad@users.noreply.github.com>
2025-08-24 13:40:58 +0300

    Update Factory.js

--- Projects/projects/Star Runner/js/patterns/Factory.js
+++ Projects/projects/Star Runner/js/patterns/Factory.js
@@ -1,12 +1,1 @@
-import Enemy from '../entities/Enemy.js';
-import PowerUp from '../entities/PowerUp.js';
 
-export default class Factory {
-    static createEnemy(x, y) {
-        return new Enemy(x, y);
-    }
-
-    static createPowerUp(x, y) {
-        return new PowerUp(x, y);
-    }
-}
