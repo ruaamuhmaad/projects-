5fa946d2f2883a97d792d50e283faf40c8845d48
RuaaBaniOdh <152775685+ruaamuhmaad@users.noreply.github.com>
2025-08-24 13:40:25 +0300

    Update PowerUp.js

--- Projects/projects/Star Runner/js/entities/PowerUp.js
+++ Projects/projects/Star Runner/js/entities/PowerUp.js
@@ -1,19 +1,1 @@
-export default class PowerUp {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.width = 20;
-        this.height = 20;
-        this.speed = 1.5;
-    }
 
-    update() {
-        this.y += this.speed;
-        if (this.y > 600) this.y = -this.height;
-    }
-
-    render(ctx) {
-        ctx.fillStyle = 'blue';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-}
