00c2a53384adb37d05dbf4918a233a262cf713a0
RuaaBaniOdh <152775685+ruaamuhmaad@users.noreply.github.com>
2025-08-24 13:43:39 +0300

    Update Factory.js

--- project5/js/Factory.js
+++ project5/js/Factory.js
@@ -1,8 +1,9 @@
-// Factory.js
+
 import { Enemy } from './Enemy.js';
 
 export class Factory {
     static createEnemies(enemyData, eventBus) {
         return enemyData.map(e => new Enemy(e.x, e.y, eventBus));
     }
+
 }
