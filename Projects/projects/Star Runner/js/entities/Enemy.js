598c86a32f1465af324e4a51d62068108a8e4c05
RuaaBaniOdh <152775685+ruaamuhmaad@users.noreply.github.com>
2025-08-24 13:39:59 +0300

    Update Enemy.js

--- Projects/projects/Star Runner/js/entities/Enemy.js
+++ Projects/projects/Star Runner/js/entities/Enemy.js
@@ -1,19 +1,1 @@
-export default class Enemy {
-    constructor(x, y) {
-        this.x = x;
-        this.y = y;
-        this.width = 40;
-        this.height = 40;
-        this.speed = 2;
-    }
 
-    update() {
-        this.y += this.speed;
-        if (this.y > 600) this.y = -this.height;
-    }
-
-    render(ctx) {
-        ctx.fillStyle = 'red';
-        ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-}
